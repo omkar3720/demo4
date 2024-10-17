be72eb20fc4723c306c57d846f822dab4dcf941a
omkar3720 <omkarshinde2728@gmail.com>
2024-10-17 10:21:43 +0530

    add

--- Crud_SpringBoot/src/main/java/com/org/crud_springboot/model/Employee.java
+++ Crud_SpringBoot/src/main/java/com/org/crud_springboot/model/Employee.java
@@ -1,5 +1,19 @@
 package com.org.crud_springboot.model;
 
-public class Employee {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Employee
+{
+  @Id
+  private int id;
+  private String name;
+  private String address;
 }
