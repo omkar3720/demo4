3cd8307381a42d1a686f36202bad53e138e226b8
omkar3720 <omkarshinde2728@gmail.com>
2024-10-17 10:29:56 +0530

    add

--- Crud_SpringBoot/src/main/java/com/org/crud_springboot/servicei/EmployeeServiceI.java
+++ Crud_SpringBoot/src/main/java/com/org/crud_springboot/servicei/EmployeeServiceI.java
@@ -1,5 +1,13 @@
 package com.org.crud_springboot.servicei;
 
+import java.util.List;
+
+import com.org.crud_springboot.model.Employee;
+
 public interface EmployeeServiceI {
+
+	public Employee saveEmployee(Employee e);
+
+	public List<Employee> getAllEmployee();
 
 }
