caf5fcfd0a72d61d26d103f0538e7862f3e3c7e7
yogesh5135 <yogesh5135@gmail.com>
2024-10-17 10:28:32 +0530

    add

--- Crud_SpringBoot/src/main/java/com/org/crud_springboot/serviceimpl/EmployeeServiceImpl.java
+++ Crud_SpringBoot/src/main/java/com/org/crud_springboot/serviceimpl/EmployeeServiceImpl.java
@@ -1,6 +1,17 @@
 package com.org.crud_springboot.serviceimpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class EmployeeServiceImpl {
+import com.org.crud_springboot.repoi.EmployeeRepositary;
+import com.org.crud_springboot.servicei.EmployeeServiceI;
 
+@Service
+public class EmployeeServiceImpl implements EmployeeServiceI {
+
+	@Autowired
+	EmployeeRepositary eri;
+	
+	
+	
 }
